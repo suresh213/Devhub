270e93efa6aa61b58ecc7b07b49d7564e2f0f3ce
sureshtech22 <asuresh52012@gmail.com>
2021-04-05 13:08:36 +0530

    production deploy

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
+
+// db connection
 connectDB();
+
 app.use(express.json({ extended: false }));
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Home');
-});
 
 app.use('/api/login', require('./routes/api/login'));
 app.use('/api/register', require('./routes/api/register'));
@@ -40,6 +41,12 @@ app.use('/api/posts/addComment', require('./routes/api/addComment'));
 app.use('/api/posts/removeComment', require('./routes/api/removeComment'));
 app.use('/api/github', require('./routes/api/github'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
