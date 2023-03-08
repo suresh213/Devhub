9991ad6b2c99049fd9fcdad103d2ee070598872e
suresh213 <asuresh52012@gmail.com>
2023-03-09 01:32:13 +0530

    Update env

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("MongoURI");
+// const db = config.get("MongoURI");
 
 const connectDB = async () => {
   try {
